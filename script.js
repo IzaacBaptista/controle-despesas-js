d9f476cf4d61c33c7398219ad37b5a94a1697319
Izaac Baptista <izaacbaptista@gmail.com>
2021-10-11 15:55:27 -0300

    const amountWithoutOperator

--- script.js
+++ script.js
@@ -8,11 +8,18 @@ const dummyTransactions = [
 const addTransactionIntoDOM = transaction => {
     const operator = transaction.amount < 0 ? '-' : '+'
     const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
+    const amountWithoutOperator = Math.abs(transaction.amount)
     const li = document.createElement('li')
     
+
+    li.classList.add(CSSClass)
+    li.innerHTML = `
+        ${transaction.name} <span>${operator} R$ <span>${Math.abs(transaction.amount)}</span><button class="delete-btn">x</button>
+
+    `
     {/*<li class="minus">
         Salário <span>-$400</span><button class="delete-btn">x</button>
     </li>*/}
 }
 
-addTransactionIntoDOM(dummyTransactions[0])
+addTransactionIntoDOM(dummyTransactions[1])
