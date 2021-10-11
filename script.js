f4fb8dae5fa7eff8c2ea9da5c270643f00236945
Izaac Baptista <izaacbaptista@gmail.com>
2021-10-11 14:56:42 -0300

    const addTransactionIntoDom

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const dummyTransactions = [
+    { id: 1, name: 'Bolo de brigadeiro', amount: -20 },
+    { id: 2, name: 'Salário', amount: 300 },
+    { id: 3, name: 'Torta de Frango', amount: -10 },
+    { id: 4, name: 'Violão', amount: 150 }
+]
+
+const addTransactionIntoDom = [
     {
         
     }
