2962ecc528ad241352740a591810f0b02e38b264
Izaac Baptista <izaacbaptista@gmail.com>
2021-10-12 20:00:20 -0300

    const form

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const transactionsUl = document.querySelector('#transactions')
+const incomeDisplay = document.querySelector('#money-plus')
+const expenseDisplay = document.querySelector('#money-minus')
+const balanceDisplay = document.querySelector('#balance')
+const form = document.querySelector('#form')
 
 const dummyTransactions = [
     { id: 1, name: 'Bolo de brigadeiro', amount: -20 },
@@ -12,17 +16,13 @@ const addTransactionIntoDOM = transaction => {
     const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
     const amountWithoutOperator = Math.abs(transaction.amount)
     const li = document.createElement('li')
-    
 
     li.classList.add(CSSClass)
     li.innerHTML = `
-        ${transaction.name} <span>${operator} R$ <span>${amountWithoutOperator}</span><button class="delete-btn">x</button>
+        ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button>
 
     `
     transactionsUl.append(li)
-
-    addTransactionIntoDOM(dummyTransactions[0])
-    addTransactionIntoDOM(dummyTransactions[1])
 }
 
 const updateBalanceValues = () => {
@@ -35,10 +35,14 @@ const updateBalanceValues = () => {
         .filter(value => value > 0)
         .reduce((accumulator, value) => accumulator + value, 0)
         .toFixed(2)
-    const expense = transactionsAmounts
+    const expense = Math.abs(transactionsAmounts
         .filter(value => value < 0)
-        .reduce((accumulator, value) => + value, 0)
+        .reduce((accumulator, value) => + value, 0))
         .toFixed(2)
+
+        balanceDisplay.textContent = `R$ ${total}`
+        incomeDisplay.textContent = `R$ ${income}`
+        expenseDisplay.textContent = `R$ ${expense}`
 }
 
 const init = () => {
